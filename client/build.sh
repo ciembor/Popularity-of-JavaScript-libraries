734c25b16ea7d4d27cae1967a261e1d1300c0e6c
ciembor <pub@ciemborowicz.pl>
2012-05-20 04:28:24 +0200

    ID can't include quotes.

--- client/build.sh
+++ client/build.sh
@@ -2,9 +2,8 @@
 
 # Generate a random id
 #  $1 = number of characters; defaults to 10
-#  $2 = include special characters; 1 = yes, 0 = no; defaults to 1
 function randid() {
-  [ "$2" == "0" ] && CHAR="[:alnum:]" || CHAR="[:graph:]"
+    CHAR="[:alnum:]"
     cat /dev/urandom | tr -cd "$CHAR" | head -c ${1:-10}
     echo
 }
